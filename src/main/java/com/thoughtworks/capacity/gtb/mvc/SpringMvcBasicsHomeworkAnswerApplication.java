b95a5f9fbc276e04f1cd17b912451bc453a99a35
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-06-16 14:07:21 +0800

    init version

---
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/SpringMvcBasicsHomeworkAnswerApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capacity.gtb.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBasicsHomeworkAnswerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMvcBasicsHomeworkAnswerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/thoughtworks/capacity/gtb/mvc/SpringMvcBasicsHomeworkAnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capacity.gtb.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBasicsHomeworkAnswerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
